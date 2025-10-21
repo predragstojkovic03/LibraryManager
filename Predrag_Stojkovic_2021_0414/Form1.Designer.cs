30db8a81de265249f02ccb8e836d329e688dab16
Predrag Stojkovic <predragstojkovic.jagodina@gmail.com>
2025-10-21 22:51:16 +0200

    feat: finished up the base server implementation

--- Predrag_Stojkovic_2021_0414/Form1.Designer.cs
+++ Predrag_Stojkovic_2021_0414/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtUsername = new TextBox();
+            txtButton = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(12, 52);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(125, 27);
+            txtUsername.TabIndex = 0;
+            // 
+            // txtButton
+            // 
+            txtButton.Location = new Point(12, 120);
+            txtButton.Name = "txtButton";
+            txtButton.Size = new Size(125, 27);
+            txtButton.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 97);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(12, 179);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(125, 41);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(417, 276);
+            Controls.Add(btnLogin);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtButton);
+            Controls.Add(txtUsername);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtUsername;
+        private TextBox txtButton;
+        private Label label1;
+        private Label label2;
+        private Button btnLogin;
     }
 }
